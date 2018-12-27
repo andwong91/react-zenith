b5f8e3f42477a5374cd3285822bb04077176dddf
Anson Wong <anwong@avery.com>
2018-12-27 14:03:55 -0800

    Adding fullscreen carousel

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,10 +1,39 @@
 import React from 'react';
-
-import StyledHero from './styled-components/StyledHero';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const ImageCarousel = (props) => {
   return (
-    <StyledHero/>
+    <section className="hero is-fullheight">
+      <div className="hero-body" style={{ padding: 0 }}>
+        <div className="container" style={{ margin: 0, minWidth: '100vw' }}>
+          <Carousel 
+            autoPlay={true}
+            infiniteLoop={true}
+            interval={6000}
+            showArrows={false}
+            showIndicators={false}
+            showStatus={false}
+            showThumbs={false}
+            stopOnHover={false}
+            swipeable={true}
+            transitionTime={600}>
+            <div>
+              <img src="assets/1.jpg" />
+              {/* <p className="legend">Legend 1</p> */}
+            </div>
+            <div>
+              <img src="assets/2.jpg" />
+              {/* <p className="legend">Legend 2</p> */}
+            </div>
+            <div>
+              <img src="assets/3.jpg" />
+              {/* <p className="legend">Legend 3</p> */}
+            </div>
+          </Carousel>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/StyledHeader.js
+++ src/components/StyledHeader.js
@@ -8,7 +8,7 @@ class Header extends Component {
 
   render() {
     return (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
+      <nav className="navbar is-transparent" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
           <a className="navbar-item" href="https://bulma.io">
             <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
